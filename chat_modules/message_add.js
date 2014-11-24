bda0f9844cb8f9be18e38b16c22f61aeda44b641
Filip Hnízdo <filip@citywebconsultants.co.uk>
2014-11-24 16:26:07 +0000

    POST messsage

--- chat_modules/message_add.js
+++ chat_modules/message_add.js
@@ -3,18 +3,34 @@
 "use strict";
 
 var exports = {
+    hook_post_message_add:{
+    rank: 1,
+    event: function(data){
+        
+        data.content = data.post.content;
+        
+        process.emit("next", data);
+        
+        process.nextTick(function(){
+        
+        process.hook("hook_message_add", data);
+        
+        });
+    }   
+    },
     hook_message_add: {
         
     rank: 1,
     event: function(data){
     
-        process.nextTick(function(){
             
-            console.log("Message received: " + data.content);
-    
-            process.emit("next", data);
-            
-        });    
+        console.log("Message received: " + data.content);
+
+        data.returns = "message received";
+        
+        process.emit("next", data);
+
+   
 }
         
         
