b926113f0f8b09a7bc88878c661897924646417f
Malachy Hall Thorpe <me@malachy.org.uk>
2014-11-26 16:26:33 +0000

    Whitespace/indentation

--- chat_modules/message_add.js
+++ chat_modules/message_add.js
@@ -4,37 +4,26 @@
 
 var exports = {
     // POST /message/add
-    hook_post_message_add:{
-    rank: 1,
-    event: function(data){
-        
-        data.content = data.post.content;
-        
-        data.returns = "message received";
+    hook_post_message_add: {
+        rank: 1,
+        event: function (data) {
+            data.content = data.post.content;
+            data.returns = "message received";
 
-        process.emit('next', data);
-        
-        process.nextTick(function(){
-                    
-        process.hook("hook_message_add", data);
-        
-        });
-    }
+            process.emit('next', data);
+
+            process.nextTick(function () {
+                process.hook("hook_message_add", data);
+            });
+        }
     },
     hook_message_add: {
-        
-    rank: 1,
-    event: function(data){
-    
+        rank: 1,
+        event: function (data) {
+            console.log("Message received: " + data.content);
+            process.emit('next', data);
             
-        console.log("Message received: " + data.content);
-        
-        process.emit('next', data);
-
-   
-}
-        
-        
+        }
     }
 };
 
