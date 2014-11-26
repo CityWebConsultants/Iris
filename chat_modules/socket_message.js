15da29f88859ef8d1687568467471e7a29b6f2fc
Filip Hnízdo <filip@bluejumpers.com>
2014-11-26 13:40:13 +0000

    Start of group fetch in socket message

--- chat_modules/socket_message.js
+++ chat_modules/socket_message.js
@@ -8,12 +8,26 @@ var exports = {
         process.nextTick(function () {
             
             process.addSocketListener("message", function (data, socket) {
+                
+                process.hook("hook_group_list_users", {groupid : data.to}, function (groupusers) {
+                    
+                    if (groupusers) {
+                     
+                        groupusers.forEach(function (element, item) {
+                         
+                            //Send message to recipient if logged in
                
-                if (process.userlist[data.to]) {
+                            if (process.userlist[data.to] && process.userlist[data.to].socket) {
                  
-                    process.userlist[data.to].socket.emit("handshake", data.content);
-                           
-                }
+                                process.userlist[data.to].socket.emit("handshake", data.content);
+                               
+                            }
+                         
+                        });
+                        
+                    }
+                    
+                });
                 
             });
             
