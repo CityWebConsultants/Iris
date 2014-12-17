2aefdd0da5b26f0a62a3b9e894dcf2d3b845a7f8
Malachy Hall Thorpe <me@malachy.org.uk>
2014-12-17 12:21:00 +0000

    add validation to socket_message.js

--- chat_modules/socket_message.js
+++ chat_modules/socket_message.js
@@ -10,52 +10,56 @@ var exports = {
         process.nextTick(function () {
             
             process.addSocketListener("message", function (data, socket) {
+                // userid, groupid, userid, content
                 
-                process.hook("hook_group_list_users", {groupid : data.to}, function (groupusers) {
-                    
-                    if (groupusers.returns) {
+                if (data.userid && data.to && data.content) {
 
-                        // userid = userid of message sender
-                        var key,
-                            userid;
-                        for (key in auth.userlist) {
-                            if (auth.userlist[key].sockets) {
+                    process.hook("hook_group_list_users", {groupid : data.to}, function (groupusers) {
 
-                                auth.userlist[key].sockets.forEach(function (element, index) {
-                                    if (element === socket) {
-                                        console.log('userid: ' + key);
-                                        userid = key;
-                                    }
-                                });
+                        if (groupusers.returns) {
 
-                                break;
+                            // userid = userid of message sender
+                            var key,
+                                userid;
+                            for (key in auth.userlist) {
+                                if (auth.userlist[key].sockets) {
+
+                                    auth.userlist[key].sockets.forEach(function (element, index) {
+                                        if (element === socket) {
+                                            console.log('userid: ' + key);
+                                            userid = key;
+                                        }
+                                    });
+
+                                    break;
+                                }
                             }
-                        }
 
-                        // Stop if there's no userid
-                        if (!userid) {
-                            process.emit('next', data);
-                        }
+                            process.hook('hook_message_add', {groupid: data.to, 'userid': userid, content: data.content, strong_auth_check: true}, function (gotData) {
 
-                        process.hook('hook_message_add', {groupid: data.to, 'userid': userid, content: data.content, strong_auth_check: true}, function (gotData) {
-                            
-                            data.messageid = gotData.returns;
+                                data.messageid = gotData.returns;
 
-                            process.hook('hook_message_process', {groupid: data.to, 'userid': userid, content: data.content}, function (gotData) {
+                                process.hook('hook_message_process', {groupid: data.to, 'userid': userid, content: data.content}, function (gotData) {
 
-                                //Clear user token. Shouldn't be sent to all users!
-                                
-                                data.token = "";
-                                
-                                process.groupBroadcast(data.to, 'message', data);
+                                    //Clear user token. Shouldn't be sent to all users!
+
+                                    data.token = "";
+
+                                    process.groupBroadcast(data.to, 'message', data);
+
+                                });
 
                             });
 
-                        });
+                        }
 
-                    }
+                    });
                     
-                });
+                // not all data sent
+                } else {
+                    console.log('bad message');
+                    process.emit('next', data);
+                }
                 
             });
             
