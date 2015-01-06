159c83a5fc83715ba492b0a9abddaa4a2753eb36
Malachy Hall Thorpe <me@malachy.org.uk>
2015-01-06 11:49:35 +0000

    Fix obscure socket duplication glitch

--- chat_modules/sockets.js
+++ chat_modules/sockets.js
@@ -6,34 +6,34 @@ var io = require('socket.io');
 var auth = require('../chat_modules/auth');
 
 var socketapi = function (socket) {
- 
+
     exports.listeners.forEach(function (element, index) {
-        
+
         var callback;
-        
+
         (function () {
 
             callback = function (data) {
-              
+
                 element.callback(data, socket);
-                
+
             };
-            
+
         }());
-       
+
         socket.on(element.event, callback);
-        
+
     });
-    
+
 };
 
 var exports = {
-    
+
     listeners: [],
     addlistener: function (listener, callback) {
-        
+
         exports.listeners.push({event : listener, callback : callback});
-        
+
     },
     groupBroadcast: function (groupid, messagename, data) {
         process.hook('hook_group_list_users', {groupid: groupid}, function (users) {
@@ -52,24 +52,24 @@ var exports = {
         });
     },
     init: function () {
-    
+
         process.nextTick(function () {
-            
+
             process.socketio = io(process.server);
             process.socketio.on("connection", function (socket) {
-                
+
                 //Add listeners
-                
+
                 socketapi(socket);
-                
+
                 //Handshake message to trigger a pair callback from client
-                
+
                 socket.emit("handshake", "Connection made");
-                  
+
                 socket.on("pair", function (data) {
-                    
+
                     process.hook("hook_auth_check", data, function (check) {
-                                                
+
                         if (check.returns) {
                             if (auth.userlist[data.userid]) {
                                 // Push socket to userlist
@@ -79,21 +79,32 @@ var exports = {
                                     auth.userlist[data.userid].sockets = [];
                                 }
 
-                                auth.userlist[data.userid].sockets.push(socket);
+                                console.log(socket.id);
 
-                                socket.userid = data.userid;
+                                // Check if socket already added
+                                var socketAlreadyExists = false;
+                                auth.userlist[data.userid].sockets.forEach(function (element, index) {
+                                    if (socket.id !== element.id) {
+                                        socketAlreadyExists = true;
+                                    }
+                                });
+
+                                if (!socketAlreadyExists) {
+                                    auth.userlist[data.userid].sockets.push(socket);
+                                    socket.userid = data.userid;
+                                }
 
                                 socket.emit('pair', true);
                             }
                         } else {
                             socket.emit('pair', false);
                         }
-                        
+
                     });
-                    
-                    
+
+
                 });
-                
+
                 socket.on("disconnect", function (reason) {
                     console.log('Disconnect from ' + socket.userid);
 
@@ -111,9 +122,9 @@ var exports = {
                 });
 
             });
-            
+
         });
-        
+
     }
 };
 
