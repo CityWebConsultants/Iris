ac8900c932e570ffa77d50d1c1535c4ede683401
Filip Hnízdo <filip@bluejumpers.com>
2015-02-09 19:13:39 +0000

    initial work on hub_sync module

---
+++ chat_modules/hub_sync.js
@@ -0,0 +1,58 @@
+/*jslint node: true nomen: true plusplus: true */
+
+"use strict";
+
+var exports = {
+    // POST /hub/sync
+    hook_post_hub_sync: {
+        rank: 1,
+        event: function (data) {
+            if (data.post.secretkey && data.post.content) {
+                process.hook('hook_secretkey_check', {
+                    secretkey: data.post.secretkey
+                }, function (check) {
+                    if (check.returns === true) {
+
+                        process.hook('hook_db_insert', {
+                            dbcollection: 'nodes',
+                            dbobject: JSON.parse(data.post.content) 
+                        }, function (gotData) {
+                            
+                            data.returns = "Updated";
+
+                            process.emit('next', data);
+
+                        })
+
+                    } else {
+                        process.emit('next', data);
+                    }
+                });
+            }
+        }
+    },
+    hook_get_hub_sync: {
+
+        rank: 0,
+        event: function (data) {
+                       
+            var query = {};
+        
+            var filter;
+            
+            for(filter in data.get){
+                
+            query[filter] = data.get[filter];
+                
+            }
+            
+            process.hook('hook_db_find', {dbcollection: 'nodes', dbquery: query}, function (gotData) {
+
+                data.returns = gotData.returns;
+                process.emit('next', data);
+            });
+        }
+    },
+};
+
+module.exports = exports;
