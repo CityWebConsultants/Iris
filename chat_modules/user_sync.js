dfa800625821236fc2ffffdfd9e405aedfa5b8ba
Filip Hnízdo <filip@bluejumpers.com>
2015-03-05 22:02:44 +0000

    User lookup functions

--- chat_modules/user_sync.js
+++ chat_modules/user_sync.js
@@ -53,55 +53,133 @@ var exports = {
 
         rank: 0,
         event: function (data) {
-            
+
             //Gets list of filters
-            
+
             var name = data.get.name;
-            
+
             name = name.split(" ");
 
             var query = [];
-            
+
             var and1 = [];
             var and2 = [];
 
-            and1.push({'field_name_last': {$regex: new RegExp('^'+name[0], "i")}});
-            and1.push({'field_name_first': {$regex: new RegExp('^'+name[0], "i")}});
-            and2.push({'field_name_last': {$regex: new RegExp('^'+name[1], "i")}});
-            and2.push({'field_name_first': {$regex: new RegExp('^'+name[1], "i")}});
-            
-            query.push({$or: and1});
-            
-            if(name[1]){
-             
-                query.push({$or: and2});
-                
+            and1.push({
+                'field_name_last': {
+                    $regex: new RegExp('^' + name[0], "i")
+                }
+            });
+            and1.push({
+                'field_name_first': {
+                    $regex: new RegExp('^' + name[0], "i")
+                }
+            });
+            and2.push({
+                'field_name_last': {
+                    $regex: new RegExp('^' + name[1], "i")
+                }
+            });
+            and2.push({
+                'field_name_first': {
+                    $regex: new RegExp('^' + name[1], "i")
+                }
+            });
+
+            query.push({
+                $or: and1
+            });
+
+            if (name[1]) {
+
+                query.push({
+                    $or: and2
+                });
+
             }
-            
-            query = {$and: query};
-            
+
+            query = {
+                $and: query
+            };
+
             process.hook('hook_db_find', {
                 dbcollection: 'users',
                 dbquery: query
             }, function (gotData) {
-                
-                var userlist = []; 
-                
-                JSON.parse(gotData.returns).forEach(function(element){
-                                        
+
+                var userlist = [];
+
+                JSON.parse(gotData.returns).forEach(function (element) {
+
                     var uid = element.uid;
                     var name = element.field_name_first + " " + element.field_name_last;
 
-                    userlist.push({uid: uid, name: name});
+                    userlist.push({
+                        uid: uid,
+                        name: name
+                    });
 
-                    
+
                 });
-                    
+
                 data.returns = JSON.stringify(userlist);
                 process.emit('next', data);
             });
         }
     },
+    hook_get_userfetch: {
+
+        rank: 0,
+        event: function (data) {
+
+            //Split data by commas
+
+            if (data.get.users) {
+
+                var query = {};
+
+                var users = data.get.users.split("+");
+
+                query["uid"] = {
+                    $in: users
+                };
+
+                process.hook('hook_db_find', {
+                    dbcollection: 'users',
+                    dbquery: query
+                }, function (gotData) {
+
+                    var userlist = gotData.returns;
+                    var output = [];
+
+                    JSON.parse(userlist).forEach(function (element) {
+
+                        var name = element.field_name_first + " " + element.field_name_last;
+                        
+                        output.push({
+                            uid: element.uid,
+                            username: name
+                        });
+
+                    });
+
+                    data.returns = JSON.stringify(output);
+                    process.emit("next", data);
+
+                });
+
+            } else {
+
+                data.returns = "Error";
+                process.emit("next", data);
+
+            };
+
+        }
+
+
+
+    }
 };
 
 module.exports = exports;
