0159edb311dc9b697e990bfe3d3365cb963a511d
Filip Hnízdo <filip@bluejumpers.com>
2015-03-16 13:54:30 +0000

    Group sync

--- chat_modules/group_sync.js
+++ chat_modules/group_sync.js
@@ -3,24 +3,12 @@
 "use strict";
 
 var exports = {
-    hook_mongodb_ready: {
-        rank: 1,
-        event: function () {
-
-            process.hook('hook_usercache', {}, function (data) {
-
-                console.log("User cache updated with " + Object.keys(process.usercache).length + " users");
-
-            });
-
-        }
-    },
     // POST /group/sync
     hook_post_group_sync: {
         rank: 1,
         event: function (data) {
-            if (data.post.secretkey && data.post.content) {
-
+            if (data.post.secretkey) {
+              
                 /*
                 Expected data:
                     content.gid: Drupal group node ID reference
@@ -28,36 +16,30 @@ var exports = {
                     content.members: Array of member user IDs
                     content.avatar: URL for group image
                 */
-
-                var content;
-
-                try {
-                    content = JSON.parse(content);
-                } catch (e) {
-                    process.emit("next", data);
-                    return;
-                }
-
                 process.hook('hook_secretkey_check', {
                     secretkey: data.post.secretkey
                 }, function (check) {
                     if (check.returns === true) {
+  
+var members = [];
+                       JSON.parse(data.post.members).forEach(function(user,index){
+  
+  members.push({userid:user});
+  
+});
 
                         process.hook('hook_db_update', {
                             dbcollection: 'groups',
                             dbquery: {
-                                gidref: content.gid
+                                gidref: data.post.gid
                             },
                             dbupdate: {
-                                name: content.name,
+                                name: data.post.name,
                                 isReadOnly: true,
                                 lastupdated: Date.now(),
-                                gidref: content.gid,
-                                avatar: content.avatar,
-                                members: {
-                                    userid: content.members,
-                                    joined: null
-                                }
+                                gidref: data.post.gid,
+                                avatar: data.post.avatar,
+                                members: members
                             },
                             dbupsert: true
                         }, function (update) {
