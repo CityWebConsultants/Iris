91afabf312c1137e4d9f460ae110b0f17dcfe885
Malachy Hall Thorpe <me@malachy.org.uk>
2015-07-07 14:51:53 +0100

    Rework hook_message_add to use promises

--- chat_modules/message_add.js
+++ chat_modules/message_add.js
@@ -149,28 +149,6 @@ var exports = {
     event: function (data) {
       console.log("[INFO] Adding message: " + JSON.stringify(data.content));
 
-      var checkprivate = function(groupid, callback) {
-        process.hook('hook_db_find', {
-          dbcollection: 'groups',
-          dbquery: {
-            _id: objectID(groupid)
-          }
-        }, function (group) {
-
-          group = JSON.parse(group.returns);
-
-          // Force messages private when it doesn't make sense for them to be public
-          if (!group.isReadOnly) {
-            callback(true);
-          } else if (group.private) {
-            callback(true);
-          } else {
-            callback(false);
-          }
-
-        });
-      }
-
       var message = {
         userid: data.userid,
         groupid: data.groupid,
@@ -180,78 +158,104 @@ var exports = {
         public: data.public
       };
 
-      process.hook('hook_message_preprocess', {
-        message: message
-      }, function (processedMessage) {
+      var preprocessMessage = function (data) {
 
-        if (processedMessage.returns) {
-          message = processedMessage.returns;
-        }
+        return new Promise(function (yes, no) {
 
-        if (data.strong_auth_check === true) {
+          process.hook('hook_message_preprocess', {
+            message: message
+          }, function (processedMessage) {
 
-          process.hook('hook_group_list_users', {
-            userid: data.userid,
-            groupid: data.groupid
-
-          }, function (gotData) {
-            var authorised = false,
-              i;
-
-            if (typeof gotData.returns !== 'string') {
-
-              // Ensure user is in group and set flag
-              for (i = 0; i < gotData.returns.length; i++) {
-                if (gotData.returns[i].userid === data.userid) {
-                  authorised = true;
-                  // No point in looping through the rest, so break
-                  break;
-                }
-              }
-
+            if (processedMessage.returns) {
+              message = processedMessage.returns;
             }
 
-            // Insert message into database
-            if (authorised === true) {
-
-              checkprivate(data.groupid, function (isPrivate) {
-
-                if (data.public && isPrivate) {
-                  message.public = false;
-                }
-
-                console.log(message);
-
-                process.hook('hook_db_insert', {
-                  dbcollection: 'messages',
-                  dbobject: message
-                }, function (gotData) {
-                  data.returns = gotData.returns[0]._id;
-
-                  if (process.usercache[message.userid]) {
-
-                    //Translate username from cache
-                    message.username = process.usercache[message.userid].username;
-                  };
-
-
-                  // Actually send message
-                  process.groupBroadcast(data.groupid, 'message', message);
-
-                  process.emit('next', data);
-                });
-
-              });
-
-            } else {
-              data.returns = false;
-              process.emit('next', data);
-            }
+            yes(data);
 
           });
 
-          // No strong auth check; insert whatever
-        } else {
+        });
+
+      };
+
+      var strongAuthCheck = function (data) {
+
+        return new Promise(function (yes, no) {
+          if (data.strong_auth_check === true) {
+
+            process.hook('hook_group_list_users', {
+              userid: data.userid,
+              groupid: data.groupid
+
+            }, function (gotData) {
+              var authorised = false,
+                i;
+
+              if (typeof gotData.returns !== 'string') {
+
+                // Ensure user is in group and set flag
+                for (i = 0; i < gotData.returns.length; i++) {
+                  if (gotData.returns[i].userid === data.userid) {
+                    authorised = true;
+                    // No point in looping through the rest, so break
+                    break;
+                  }
+                }
+
+              }
+
+              // Insert message into database
+              if (authorised === true) {
+
+                yes(data);
+
+              } else {
+
+                data.returns = false;
+                data.errors.push("Not authorised to post to that group.");
+                no(data);
+
+              }
+
+            });
+
+          } else {
+
+            console.log("[INFO] Strong auth check bypassed.");
+
+            yes(data);
+
+          }
+
+        });
+
+      };
+
+      var insertMessageDB = function(data) {
+
+        return new Promise(function (yes, no) {
+
+          var checkprivate = function(groupid, callback) {
+            process.hook('hook_db_find', {
+              dbcollection: 'groups',
+              dbquery: {
+                _id: objectID(groupid)
+              }
+            }, function (group) {
+
+              group = JSON.parse(group.returns);
+
+              // Force messages private when it doesn't make sense for them to be public
+              if (!group.isReadOnly) {
+                callback(true);
+              } else if (group.private) {
+                callback(true);
+              } else {
+                callback(false);
+              }
+
+            });
+          };
 
           checkprivate(data.groupid, function (isPrivate) {
 
@@ -261,31 +265,34 @@ var exports = {
 
             console.log(message);
 
-            console.log('[INFO] Strong authorisation check bypassed.');
             process.hook('hook_db_insert', {
               dbcollection: 'messages',
               dbobject: message
             }, function (gotData) {
               data.returns = gotData.returns[0]._id;
 
-              //Translate username from cache
-
               if (process.usercache[message.userid]) {
 
+                //Translate username from cache
                 message.username = process.usercache[message.userid].username;
-
               };
 
-              // Actually send message
 
+              // Actually send message via sockets
               process.groupBroadcast(data.groupid, 'message', message);
 
-              process.emit('next', data);
+              yes(data);
+
             });
 
           });
-        }
-      });
+
+        });
+
+      };
+
+      process.hookPromiseChain([preprocessMessage, strongAuthCheck, insertMessageDB], data);
+
     }
   }
 };
