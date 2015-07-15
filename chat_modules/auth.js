8dadf221f23fbeb76ca987f022ada012c43b63d1
Filip Hnízdo <filip@bluejumpers.com>
2015-07-15 16:39:52 +0100

    New quick, sync, authcheck function

--- chat_modules/auth.js
+++ chat_modules/auth.js
@@ -15,181 +15,207 @@
 var crypto = require('crypto');
 
 var exports = {
-    //List of logged in users/access tokens
-    userlist: {},
-    // A side effect of declaring this object is that you can have default options!
-    options: {token_length: 16, allowdebug: false},
-    // Global functions
-    globals: {
-      authCheck: function (data) {
+  //List of logged in users/access tokens
+  userlist: {},
+  // A side effect of declaring this object is that you can have default options!
+  options: {
+    token_length: 16,
+    allowdebug: false
+  },
+  // Global functions
+  globals: {
+    authCheck2: function (userid, token) {
 
-        return new Promise(function (yes, no) {
+      var user = exports.userlist[userid],
+        token = token,
+        authenticated = false;
 
-          hook('hook_auth_check', {
-            userid: data.userid,
-            token: data.token
-          }, function (gotData) {
-            if (gotData.returns === true) {
+      if (user) {
 
-              yes(data);
+        //Loop over tokens
 
-            } else {
+        user.tokens.forEach(function (element) {
 
-              data.returns = "ERROR: Authentication failed.";
-              no(data);
+          if (token === element) {
 
-            }
-          });
+            authenticated = true;
+
+          }
 
         });
 
+      } else {
+
+        authenticated = true;
+
       }
+
+      return authenticated;
+
     },
-    // POST /auth
-    hook_post_auth: {
-        rank: 0,
-        event:
-            function (data) {
+    authCheck: function (data) {
 
-                var authToken;
+      return new Promise(function (yes, no) {
 
-                if (data.post.userid && data.post.apikey && data.post.secretkey) {
+        hook('hook_auth_check', {
+          userid: data.userid,
+          token: data.token
+        }, function (gotData) {
+          if (gotData.returns === true) {
 
-                    hook("hook_secretkey_check", {
-                        apikey: data.post.apikey,
-                        secretkey: data.post.secretkey
-                    }, function (check) {
+            yes(data);
 
-                        if (check.returns) {
-                            crypto.randomBytes(exports.options.token_length, function (ex, buf) {
-                                authToken = buf.toString('hex');
+          } else {
 
-                                //Create new user if not in existence
+            data.returns = "ERROR: Authentication failed.";
+            no(data);
 
-                                if (!exports.userlist[data.post.userid]) {
-                                    exports.userlist[data.post.userid] = {};
+          }
+        });
 
-                                }
+      });
 
-                                //Check if no tokens already set and create array
-
-                                if (!exports.userlist[data.post.userid].tokens) {
-                                    exports.userlist[data.post.userid].tokens = [];
-                                }
-
-                                exports.userlist[data.post.userid].tokens.push(authToken);
-                                data.returns = authToken;
-                                process.emit('next', data);
-                            });
-                        } else {
-                            data.returns = "Secret key pair invalid.";
-                            process.emit("next", data);
-                        }
-
-                    });
-
-                } else {
-                    data.returns = "ERROR: Not all data provided";
-                    process.emit('next', data);
-                }
-
-            }
-    },
-    hook_auth_check: {
-        rank: 0,
-        event:
-            function (data) {
-                var user = exports.userlist[data.userid],
-                    token = data.token,
-                    authenticated = false;
-
-                if (user) {
-
-                    //Loop over tokens
-
-                    user.tokens.forEach(function (element) {
-
-                        if (token === element) {
-
-                            authenticated = true;
-
-                        }
-
-                    });
-
-                    data.returns = authenticated;
-
-                    process.emit('next', data);
-
-                } else {
-
-                    data.returns = false;
-                    process.emit('next', data);
-
-                }
-
-            }
-    },
-    hook_secretkey_check: {
-        rank: 0,
-        event:
-            function (data) {
-                // secretkey only
-                if (data.secretkey === process.config.secretkey && data.apikey === process.config.apikey) {
-                    data.returns = true;
-                    process.emit('next', data);
-                } else {
-                    data.returns = false;
-                    process.emit('next', data);
-                }
-            }
-    },
-    // GET /debug/isauth
-    hook_get_debug_isauth: {
-        rank: 0,
-        event:
-            function (data) {
-                if (exports.options && exports.options.allowdebug) {
-                    var userid = data.get.userid,
-                        token = data.get.token;
-
-                    // Call auth_check hook
-                    hook('hook_auth_check',
-                        {
-                            'userid': userid,
-                            'token': token,
-                            callback: function (gotData) {
-                                data.returns = JSON.stringify(gotData.returns);
-                            }
-                        });
-
-                    process.emit('next', data);
-
-                } else {
-                    data.returns = 'ERROR: Feature disabled.';
-                    process.emit('next', data);
-                }
-            }
-    },
-    // GET /debug/userlist
-    hook_get_debug_userlist: {
-        rank: 0,
-        event:
-            function (data) {
-                var index;
-
-                if (exports.options && exports.options.allowdebug) {
-
-                    data.returns = JSON.stringify(Object.keys(exports.userlist));
-
-                    process.emit('next', data);
-
-                } else {
-                    data.returns = 'ERROR: Feature disabled.';
-                    process.emit('next', data);
-                }
-            }
     }
+  },
+  // POST /auth
+  hook_post_auth: {
+    rank: 0,
+    event: function (data) {
+
+      var authToken;
+
+      if (data.post.userid && data.post.apikey && data.post.secretkey) {
+
+        hook("hook_secretkey_check", {
+          apikey: data.post.apikey,
+          secretkey: data.post.secretkey
+        }, function (check) {
+
+          if (check.returns) {
+            crypto.randomBytes(exports.options.token_length, function (ex, buf) {
+              authToken = buf.toString('hex');
+
+              //Create new user if not in existence
+
+              if (!exports.userlist[data.post.userid]) {
+                exports.userlist[data.post.userid] = {};
+
+              }
+
+              //Check if no tokens already set and create array
+
+              if (!exports.userlist[data.post.userid].tokens) {
+                exports.userlist[data.post.userid].tokens = [];
+              }
+
+              exports.userlist[data.post.userid].tokens.push(authToken);
+              data.returns = authToken;
+              process.emit('next', data);
+            });
+          } else {
+            data.returns = "Secret key pair invalid.";
+            process.emit("next", data);
+          }
+
+        });
+
+      } else {
+        data.returns = "ERROR: Not all data provided";
+        process.emit('next', data);
+      }
+
+    }
+  },
+  hook_auth_check: {
+    rank: 0,
+    event: function (data) {
+      var user = exports.userlist[data.userid],
+        token = data.token,
+        authenticated = false;
+
+      if (user) {
+
+        //Loop over tokens
+
+        user.tokens.forEach(function (element) {
+
+          if (token === element) {
+
+            authenticated = true;
+
+          }
+
+        });
+
+        data.returns = authenticated;
+
+        process.emit('next', data);
+
+      } else {
+
+        data.returns = false;
+        process.emit('next', data);
+
+      }
+
+    }
+  },
+  hook_secretkey_check: {
+    rank: 0,
+    event: function (data) {
+      // secretkey only
+      if (data.secretkey === process.config.secretkey && data.apikey === process.config.apikey) {
+        data.returns = true;
+        process.emit('next', data);
+      } else {
+        data.returns = false;
+        process.emit('next', data);
+      }
+    }
+  },
+  // GET /debug/isauth
+  hook_get_debug_isauth: {
+    rank: 0,
+    event: function (data) {
+      if (exports.options && exports.options.allowdebug) {
+        var userid = data.get.userid,
+          token = data.get.token;
+
+        // Call auth_check hook
+        hook('hook_auth_check', {
+          'userid': userid,
+          'token': token,
+          callback: function (gotData) {
+            data.returns = JSON.stringify(gotData.returns);
+          }
+        });
+
+        process.emit('next', data);
+
+      } else {
+        data.returns = 'ERROR: Feature disabled.';
+        process.emit('next', data);
+      }
+    }
+  },
+  // GET /debug/userlist
+  hook_get_debug_userlist: {
+    rank: 0,
+    event: function (data) {
+      var index;
+
+      if (exports.options && exports.options.allowdebug) {
+
+        data.returns = JSON.stringify(Object.keys(exports.userlist));
+
+        process.emit('next', data);
+
+      } else {
+        data.returns = 'ERROR: Feature disabled.';
+        process.emit('next', data);
+      }
+    }
+  }
 };
 
 process.userlist = exports.userlist;
