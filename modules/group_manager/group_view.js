19ad60a215f546ca563bc5469cb83d7cfcd78a4d
Filip Hnízdo <filip@bluejumpers.com>
2015-08-28 14:07:33 +0100

    Remove group view

--- modules/group_manager/group_view.js
+++ modules/group_manager/group_view.js
@@ -2,11 +2,11 @@ CM.group_manager.registerHook("hook_entity_view_group", 0, function (thisHook, d
 
   //Strip out IDs
 
-  data.forEach(function (group, index) {
-
-    data[index]._id = undefined;
-
-  });
+//  data.forEach(function (group, index) {
+//
+//    data[index]._id = undefined;
+//
+//  });
 
   thisHook.finish(true, data);
 
