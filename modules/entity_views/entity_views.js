8e0b590c9e4af2786aab3b6ecb68aa733b04cbc0
Malachy Hall Thorpe <me@malachy.org.uk>
2015-09-10 16:13:48 +0100

    If view hook n/a, still post entityCreate and entityUpdate

--- modules/entity_views/entity_views.js
+++ modules/entity_views/entity_views.js
@@ -14,17 +14,29 @@ CM.entity_views.registerHook("hook_entity_created", 0, function (thisHook, data)
 
   }, function (fail) {
 
-    console.log(fail);
+    if (fail === "No such hook exists") {
+
+      C.sendSocketMessage(["*"], "entityCreate", data);
+
+    }
 
   });
 
 });
 
 CM.entity_views.registerHook("hook_entity_updated", 0, function (thisHook, data) {
-  
+
   C.hook("hook_entity_view_" + data.entityType, [data], thisHook.authPass).then(function (filtered) {
 
     C.sendSocketMessage(["*"], "entityUpdate", filtered[0]);
+
+  }, function (fail) {
+
+    if (fail === "No such hook exists") {
+
+      C.sendSocketMessage(["*"], "entityUpdate", data);
+
+    }
 
   });
 
