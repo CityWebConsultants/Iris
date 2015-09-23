5dfb622f6d62526a0af1c37d54c969942e05d734
Malachy Hall Thorpe <me@malachy.org.uk>
2015-09-23 10:27:32 +0100

    Merge templates.js

--- modules/entity_views/static/templates.js
+++ modules/entity_views/static/templates.js
@@ -1,6 +1,19 @@
 var T = {};
 
-T.newData = new Event('newdata');
+(function () {
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   }
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+})();
+
+T.newData = new CustomEvent('newdata');
 
 T.observer = new MutationObserver(function (mutations) {
   mutations.forEach(function (mutation) {
