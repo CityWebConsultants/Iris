866b7ab83eb02136e805d4571f7c76d6cdeafe93
Filip Hnízdo <filip@bluejumpers.com>
2015-09-29 13:24:53 +0100

    Cascading schema files

--- db.js
+++ db.js
@@ -158,7 +158,7 @@ var unstringifySchema = function (field) {
 };
 
 C.registerDbSchema = function (model, schema) {
-  
+
   if (typeof model === "string" && typeof schema === "object") {
 
     //Loop over provided schema fields
@@ -192,23 +192,76 @@ C.registerDbSchema = function (model, schema) {
 
 C.dbCollections = {};
 
+C.dbSchema = {};
+
 C.dbPopulate = function () {
 
-  //Loop over all the db models that have been initialised and slot in any schema attached to them
+  // Loop over all enabled modules and check for schema files
+
+  Object.keys(CM).forEach(function (moduleName) {
+
+    try {
+      fs.readdirSync(CM[moduleName].path + "/schema").forEach(function (schemafile) {
+
+        schemafile = schemafile.toLowerCase().replace(".json", "");
+
+        //Check if schema already exists for entity type, if not, add it
+
+        if (!C.dbSchema[schemafile]) {
+
+          C.dbSchema[schemafile] = {};
+
+        }
+
+        var file = JSON.parse(fs.readFileSync(CM[moduleName].path + "/schema/" + schemafile + ".json"));
+
+        Object.keys(file).forEach(function (field) {
+
+          C.dbSchema[schemafile][field] = file[field];
+
+        });
+
+      });
+
+    } catch (e) {
+
+
+    }
+
+
+  });
+
+  // See if site config has added any schema or schemafields
 
   fs.readdirSync(C.sitePath + "/configurations/entity").forEach(function (schemafile) {
 
-    var model = schemafile.replace(".JSON", "");
+    var schemaName = schemafile.toLowerCase().replace(".json", "");
 
-    var schema = fs.readFileSync(C.sitePath + "/configurations/entity/" + schemafile, "UTF8");
+    var file = JSON.parse(fs.readFileSync(C.sitePath + "/configurations/entity/" + schemafile, "UTF8"));
 
-    schema = JSON.parse(schema);
+    if (!C.dbSchema[schemaName]) {
 
-    Object.keys(schema).forEach(function (thisField) {
-            
-      unstringifySchema(schema[thisField]);
+      C.dbSchema[schemaName] = {};
 
-    })
+    }
+
+    Object.keys(file).forEach(function (field) {
+
+      C.dbSchema[schemaName][field] = file[field];
+
+    });
+
+  });
+
+  // Schema ready, now unstringify it and save it as a database model
+
+  Object.keys(C.dbSchema).forEach(function (schema) {
+
+    Object.keys(C.dbSchema[schema]).forEach(function (field) {
+  
+      unstringifySchema(C.dbSchema[schema][field]);
+
+    });
 
     //Push in author and entity type fields
 
@@ -226,25 +279,35 @@ C.dbPopulate = function () {
       required: true
     }
 
-    var schema = mongoose.Schema(schema);
+    try {
+      var readySchema = mongoose.Schema(C.dbSchema[schema]);
 
-    if (mongoose.models[model]) {
 
-      delete mongoose.models[model];
+      if (mongoose.models[schema]) {
+
+        delete mongoose.models[schema];
+
+      }
+
+      C.dbCollections[schema] = mongoose.model(schema, readySchema);
+
+    } catch (e) {
+
+      console.log(e);
 
     }
-
-    C.dbCollections[model] = mongoose.model(model, schema);
+    
+    console.log(C.dbCollections);
 
     //Create permissions for this entity type
 
-    CM.auth.globals.registerPermission("can create " + model, "entity")
-    CM.auth.globals.registerPermission("can edit any " + model, "entity")
-    CM.auth.globals.registerPermission("can edit own " + model, "entity")
-    CM.auth.globals.registerPermission("can view any " + model, "entity")
-    CM.auth.globals.registerPermission("can view own " + model, "entity")
-    CM.auth.globals.registerPermission("can delete any " + model, "entity")
-    CM.auth.globals.registerPermission("can delete own " + model, "entity")
+    CM.auth.globals.registerPermission("can create " + schema, "entity")
+    CM.auth.globals.registerPermission("can edit any " + schema, "entity")
+    CM.auth.globals.registerPermission("can edit own " + schema, "entity")
+    CM.auth.globals.registerPermission("can view any " + schema, "entity")
+    CM.auth.globals.registerPermission("can view own " + schema, "entity")
+    CM.auth.globals.registerPermission("can delete any " + schema, "entity")
+    CM.auth.globals.registerPermission("can delete own " + schema, "entity")
 
   });
 
