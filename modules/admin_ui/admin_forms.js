d5f96cd6ca549d0d08dd84301e22d54dc9f20be8
Malachy Hall Thorpe <me@malachy.org.uk>
2015-10-20 10:36:59 +0100

    Remove old stuff from admin forms

--- modules/admin_ui/admin_forms.js
+++ modules/admin_ui/admin_forms.js
@@ -1,16 +1,2 @@
 
-// Note - form preprocess
 
-//CM.admin_ui.registerHook("hook_form_schema_alter_adminlogin", 0, function (thisHook, data) {
-//
-//  data.schema["hello"] = {
-//
-//    type: "string",
-//    title: "New",
-//    required: false
-//
-//  }
-//
-//  thisHook.finish(true, data);
-//
-//})
