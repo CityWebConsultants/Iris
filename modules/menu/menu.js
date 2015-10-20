3abf1fb85d88b0af83a41b0edf817b9db74a81b6
Filip Hnízdo <filip@bluejumpers.com>
2015-10-20 17:10:17 +0100

    Function for registering menu items

--- modules/menu/menu.js
+++ modules/menu/menu.js
@@ -3,7 +3,7 @@ C.registerModule("menu");
 CM.menu.registerHook("hook_frontend_template_parse", 0, function (thisHook, data) {
 
   CM.frontend.globals.parseBlock("menu", data.html, function (menu, next) {
-    
+
     CM.frontend.globals.findTemplate(["menu", menu]).then(function (yes) {
 
       next(yes);
@@ -15,7 +15,7 @@ CM.menu.registerHook("hook_frontend_template_parse", 0, function (thisHook, data
     });
 
   }).then(function (html) {
-    
+
     data.html = html;
 
     thisHook.finish(true, data);
@@ -28,3 +28,57 @@ CM.menu.registerHook("hook_frontend_template_parse", 0, function (thisHook, data
   });
 
 });
+
+CM.menu.globals.registerMenuItem = function (menuName, path, title) {
+
+  C.dbCollections.menu.findOne({
+    'title': menuName
+  }, function (err, doc) {
+
+    if (doc) {
+
+      if (doc.menulink.some(function (currentValue, index) {
+
+          return currentValue.path === path;
+
+        })) {
+
+        return false;
+
+      }
+
+      C.dbCollections.menu.update({
+          "title": menuName
+        }, {
+          "$addToSet": {
+            "menulink": {
+              "title": "hello",
+              "path": "/"
+            }
+          }
+        }, {
+          "upsert": true
+        },
+        function (err, data) {
+
+          console.log(err, data);
+
+        }
+      )
+
+    } else {
+
+      var entity = new C.dbCollections.menu({
+        title: menuName,
+        entityAuthor: "system",
+        entityType: "menu",
+        menulink: [{
+          title: title,
+          path: path
+              }]
+      });
+
+    }
+
+  })
+};
