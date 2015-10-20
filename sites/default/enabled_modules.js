e6685252d5bbb96a39e2a881a630c8fd2d2c7ae5
Filip Hnízdo <filip@bluejumpers.com>
2015-10-20 09:40:41 +0100

    Default site folder

--- sites/default/enabled_modules.js
+++
@@ -1,9 +0,0 @@
-require(C.rootPath + '/modules/group_manager/group_manager.js');
-require(C.rootPath + '/modules/messages/messages.js');
-require(C.rootPath + '/modules/messages_replies/messages_replies.js');
-require(C.rootPath + '/modules/entity_views/entity_views.js');
-require(C.rootPath + '/modules/admin/admin.js');
-require(C.rootPath + '/modules/chat_message/chat_message.js');
-require(C.rootPath + '/modules/about/about.js');
-require(C.rootPath + '/modules/actions/actions.js');
-require(C.rootPath + '/modules/frontend/frontend.js');
