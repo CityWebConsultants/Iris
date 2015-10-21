169daece7655f64fd8ce73b5a4b03041a9d22efe
Malachy Hall Thorpe <me@malachy.org.uk>
2015-10-21 13:08:32 +0100

    Admin ui menu items

--- modules/admin_ui/admin_ui.js
+++ modules/admin_ui/admin_ui.js
@@ -6,3 +6,8 @@ CM.frontend.globals.templateRegistry.external.push(__dirname + '/templates');
 require('./admin_modules.js');
 
 require('./admin_routing.js');
+
+CM.menu.globals.registerMenuItem('admin_toolbar', '/admin/entities', 'Entities');
+CM.menu.globals.registerMenuItem('admin_toolbar', '/admin/regions', 'Regions');
+CM.menu.globals.registerMenuItem('admin_toolbar', '/admin/permissions', 'Permissions');
+CM.menu.globals.registerMenuItem('admin_toolbar', '/admin/logs', 'Logs');
