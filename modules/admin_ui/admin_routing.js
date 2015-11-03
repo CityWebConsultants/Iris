4dac9588eb0ae98ede7d4372c635143c93dacd6d
Malachy Hall Thorpe <me@malachy.org.uk>
2015-11-03 15:00:43 +0000

    Add admin home/dashboard page

--- modules/admin_ui/admin_routing.js
+++ modules/admin_ui/admin_routing.js
@@ -62,6 +62,7 @@ CM.admin_ui.globals.registerPath = function (path, templateName) {
 }
 
 CM.admin_ui.globals.registerPath("/admin/login", "admin_login");
+CM.admin_ui.globals.registerPath("/admin", "admin_dashboard");
 CM.admin_ui.globals.registerPath("/admin/permissions", "admin_permissions");
 CM.admin_ui.globals.registerPath("/admin/entities", "admin_entity_types");
 CM.admin_ui.globals.registerPath("/admin/schema/create", "admin_schema_create");
