d9e3b36c2247c36203c81b30c6216538e3f6d508
Filip Hnízdo <filip@bluejumpers.com>
2015-11-03 13:51:34 +0000

    Config import export file (not ignored now)

---
+++ modules/admin_ui/admin_modules/config.js
@@ -0,0 +1,23 @@
+var ncp = require('ncp').ncp;
+
+C.app.get("/admin/config/export", function (req, res) {
+
+  ncp(C.configPath, C.sitePath + "/staging", function (err) {
+    if (err) {
+      return console.error(err);
+    }
+    res.send("Config exported")
+  });
+
+});
+
+C.app.get("/admin/config/import", function (req, res) {
+
+  ncp(C.sitePath + "/staging", C.configPath, function (err) {
+    if (err) {
+      return console.error(err);
+    }
+    res.send("Config imported")
+  });
+
+});
