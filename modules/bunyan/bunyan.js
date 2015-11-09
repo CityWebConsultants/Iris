3a7a2deca3485acbaaa1a533a62c7f1ab4bc9e97
Malachy Hall Thorpe <me@malachy.org.uk>
2015-11-09 15:37:48 +0000

    Base for bunyan settings; pretty console log in bunyan module

--- modules/bunyan/bunyan.js
+++ modules/bunyan/bunyan.js
@@ -10,22 +10,50 @@ var mkdirSync = function (path) {
   }
 }
 
+var settings = {
+  logToFile: 'warn',
+  logToConsole: 'warn',
+  logFile: 'logs/main.log'
+}
+
+var logLevels = {
+  'trace': 10,
+  'debug': 20,
+  'info': 30,
+  'warn': 40,
+  'error': 50,
+  'fatal': 60
+}
+
 mkdirSync(C.sitePath + "/" + "logs");
 
 var bunyan = require('bunyan');
 
-CM.bunyan.globals.logger = bunyan.createLogger({
+var bunyanSettings = {
   name: 'C',
-  streams: [{
-    path: C.sitePath + "/" + "logs/main.log",
-  }]
-});
+  streams: []
+};
+
+if (settings.logToFile && settings.logFile) {
+  bunyanSettings.streams.push({level: settings.logToFile, path: C.sitePath + '/' + settings.logFile});
+}
+
+CM.bunyan.globals.logger = bunyan.createLogger(bunyanSettings);
 
 CM.bunyan.registerHook("hook_log", 0, function (thisHook, data) {
 
   CM.bunyan.globals.logger[thisHook.const.type](thisHook.const.message);
 
-}); C.log("error", "tes2t");
+  if (settings.logToConsole && logLevels[thisHook.const.type] >= logLevels[settings.logToConsole]) {
+
+    var time = new Date();
+    var timeString = ("0" + time.getHours()).slice(-2)   + ":" + ("0" + time.getMinutes()).slice(-2) + ":" + ("0" + time.getSeconds()).slice(-2);
+
+    console.log(timeString + ' [' + thisHook.const.type.toUpperCase() + '] ' + thisHook.const.message);
+
+  }
+
+});
 
 CM.auth.globals.registerPermission("can read logs", "logs");
 
