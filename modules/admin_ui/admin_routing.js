75b545fc6082eedc97d3c651775297fd28bfc6bc
Malachy Hall Thorpe <me@malachy.org.uk>
2015-11-10 15:43:50 +0000

    New entitylist in admin

--- modules/admin_ui/admin_routing.js
+++ modules/admin_ui/admin_routing.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-CM.admin_ui.globals.registerPath = function (path, templateName) {
+CM.admin_ui.globals.registerPath = function (path, templateName, prepFunction) {
 
   C.app.get(path, function (req, res) {
 
@@ -35,25 +35,78 @@ CM.admin_ui.globals.registerPath = function (path, templateName) {
 
     }
 
-    CM.frontend.globals.findTemplate([templateName], "html").then(function (html) {
+    if (!prepFunction) {
 
-      var admin_wrapper = fs.readFileSync(__dirname + '/templates/admin.html', "utf8");
+      prepFunction = function (req, callback) {
 
-      html = admin_wrapper.replace('[[[ADMINCONTENT]]]', html);
+        callback({test: 'tes'});
 
-      CM.frontend.globals.parseTemplate(html, req.authPass).then(function (page) {
+      }
 
-        res.send(page.html);
+    }
+
+    prepFunction(req, function (variables) {
+
+      // find specified template
+
+      CM.frontend.globals.findTemplate([templateName], "html").then(function (html) {
+
+        // Merge in admin wrapper (including admin menu)
+
+        var admin_wrapper = fs.readFileSync(__dirname + '/templates/admin.html', "utf8");
+
+        html = admin_wrapper.replace('[[[ADMINCONTENT]]]', html);
+
+        // Parse using the templating system
+
+        CM.frontend.globals.parseTemplate(html, req.authPass).then(function (page) {
+
+          page.variables = Object.assign(variables, page.variables);
+
+          // Parse with Mustache manually
+          // ----------------------------
+          // Does not use the hook to do this as admin_ui needs to work independently
+          // of which templating system is used for the site frontend.
+
+          var Mustache = require('mustache');
+
+          try {
+
+            page.html = Mustache.render(page.html, page.variables);
+
+            res.send(page.html);
+
+          } catch (e) {
+
+            C.log("error", "Mustache rendering failed");
+
+            C.hook("hook_display_error_page", req.authPass, {
+              error: 500,
+              req: req,
+              res: res
+            }).then(function (success) {
+
+              res.status(500).send(success);
+
+            }, function (fail) {
+
+              res.status(500).send("Something went wrong");;
+
+            });
+
+          }
+
+        });
+
+      }, function () {
+
+        var error = function (req, res) {
+
+          res.send("Error loading template");
+
+        };
 
       });
-
-    }, function () {
-
-      var error = function (req, res) {
-
-        res.send("Error loading template");
-
-      };
 
     });
 
@@ -61,10 +114,57 @@ CM.admin_ui.globals.registerPath = function (path, templateName) {
 
 }
 
+var prepareEntitylist = function (req, callback) {
+
+  // Query for all entities of this type
+
+  if (C.dbCollections[req.params.type]) {
+
+    var fields = [];
+
+//    Object.keys(C.dbCollections[req.params.type].schema.tree).forEach(function (fieldTitle) {
+//
+//      var field = C.dbCollections[req.params.type].schema.tree[fieldTitle];
+//
+//      if (fieldTitle !== "entityType" && field.type === String && field.long !== true) {
+//
+//        fields.push(fieldTitle);
+//
+//      };
+//
+//    });
+
+    C.dbCollections[req.params.type].find({}, function (err, doc) {
+
+      if (!err) {
+
+        callback({entities: doc, fields: fields});
+
+      } else {
+
+        C.log("error", "Database error while fetching entities");
+
+        callback({});
+
+      }
+
+    })
+
+  } else {
+
+    C.log("error", "Request for invalid entity type");
+
+    callback({});
+
+  }
+
+}
+
 CM.admin_ui.globals.registerPath("/admin/login", "admin_login");
 CM.admin_ui.globals.registerPath("/admin", "admin_dashboard");
 CM.admin_ui.globals.registerPath("/admin/permissions", "admin_permissions");
 CM.admin_ui.globals.registerPath("/admin/entities", "admin_entity_types");
+CM.admin_ui.globals.registerPath("/admin/entitylist2/:type", "admin_entitylist", prepareEntitylist);
 CM.admin_ui.globals.registerPath("/admin/schema/create", "admin_schema_create");
 CM.admin_ui.globals.registerPath("/admin/schema/edit/:type", "admin_schema_edit");
 CM.admin_ui.globals.registerPath("/admin/edit/:type/:_id", "admin_entity_edit");
