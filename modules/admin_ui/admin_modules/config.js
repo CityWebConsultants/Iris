f32ed0e214aac56e8a16696bc60f2f660e947fdc
Malachy Hall Thorpe <me@malachy.org.uk>
2015-11-13 09:41:37 +0000

    Add pages for import/export config confirmation

--- modules/admin_ui/admin_modules/config.js
+++ modules/admin_ui/admin_modules/config.js
@@ -1,6 +1,6 @@
 var ncp = require('ncp').ncp;
 
-C.app.get("/admin/config/export", function (req, res) {
+C.app.get("/admin/api/config/export", function (req, res) {
 
   ncp(C.configPath, C.sitePath + "/staging", function (err) {
     if (err) {
@@ -11,7 +11,7 @@ C.app.get("/admin/config/export", function (req, res) {
 
 });
 
-C.app.get("/admin/config/import", function (req, res) {
+C.app.get("/admin/api/config/import", function (req, res) {
 
   ncp(C.sitePath + "/staging", C.configPath, function (err) {
     if (err) {
