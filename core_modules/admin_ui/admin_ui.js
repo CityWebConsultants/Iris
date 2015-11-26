891f5531432ce7f3c197999fd6e8d5df591092b4
Filip Hnízdo <filip@bluejumpers.com>
2015-11-26 12:07:15 +0000

    Simple drop down admin menu

--- core_modules/admin_ui/admin_ui.js
+++ core_modules/admin_ui/admin_ui.js
@@ -4,26 +4,31 @@ require('./admin_modules.js');
 
 require('./admin_routing.js');
 
-CM.menu.globals.registerMenu("admin_toolbar");
+CM.menu.globals.registerMenu("admin-toolbar");
 
-CM.menu.globals.registerMenuLink("admin_toolbar", null, "/admin/entities", "Entities");
+CM.menu.globals.registerMenuLink("admin-toolbar", null, "/admin/entities", "Entities");
 
-CM.menu.globals.registerMenuLink("admin_toolbar", "/admin/entities", "/admin/test", "Test");
+CM.menu.globals.registerMenuLink("admin-toolbar", null, "/admin/structure", "Structure");
 
-CM.menu.globals.registerMenuLink("admin_toolbar", null, "/admin/regions", "Regions");
-CM.menu.globals.registerMenuLink("admin_toolbar", null, "/admin/blocks", "Blocks");
-CM.menu.globals.registerMenuLink("admin_toolbar", null, "/admin/permissions", "Permissions");
-CM.menu.globals.registerMenuLink("admin_toolbar", null, "/admin/menu", "Menus");
-CM.menu.globals.registerMenuLink("admin_toolbar", null, "/admin/config/export", "Export config");
-CM.menu.globals.registerMenuLink("admin_toolbar", null, "/admin/config/import", "Import config");
-CM.menu.globals.registerMenuLink("admin_toolbar", null, "/admin/logs", "Logs");
-CM.menu.globals.registerMenuLink("admin_toolbar", null, "/logout", "Log Out");
+CM.menu.globals.registerMenuLink("admin-toolbar", "/admin/structure", "/admin/regions", "Regions");
+CM.menu.globals.registerMenuLink("admin-toolbar", "/admin/structure", "/admin/blocks", "Blocks");
+CM.menu.globals.registerMenuLink("admin-toolbar", "/admin/structure", "/admin/menu", "Menus");
+
+CM.menu.globals.registerMenuLink("admin-toolbar", null, "/admin/config", "Config");
+
+CM.menu.globals.registerMenuLink("admin-toolbar", "/admin/config", "/admin/config/export", "Export config");
+CM.menu.globals.registerMenuLink("admin-toolbar", "/admin/config", "/admin/config/import", "Import config");
+
+CM.menu.globals.registerMenuLink("admin-toolbar", null, "/admin/permissions", "Permissions");
+
+CM.menu.globals.registerMenuLink("admin-toolbar", null, "/admin/logs", "Logs");
+CM.menu.globals.registerMenuLink("admin-toolbar", null, "/logout", "Log Out");
 
 CM.auth.globals.registerPermission("can view admin menu", "admin");
 
 CM.admin_ui.registerHook("hook_menu_view", 1, function (thisHook, menuName) {
 
-  if (menuName !== "admin_toolbar") {
+  if (menuName !== "admin-toolbar") {
 
     thisHook.finish(true, menuName);
 
