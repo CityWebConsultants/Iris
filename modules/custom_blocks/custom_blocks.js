20229c2874b037e00142d2dc52c1080224362bcb
Malachy Hall Thorpe <me@malachy.org.uk>
2015-12-01 14:07:46 +0000

    Remove Template-file type of block (modules can implement this themselves)

--- modules/custom_blocks/custom_blocks.js
+++ modules/custom_blocks/custom_blocks.js
@@ -1,38 +1,6 @@
 C.registerModule("custom_blocks");
 
-CM.blocks.globals.registerBlockType("Template-file");
 CM.blocks.globals.registerBlockType("Custom-HTML");
-
-CM.custom_blocks.registerHook("hook_form_render_blockForm_Template-file", 0, function (thisHook, data) {
-
-  var currentTemplate = '';
-
-  if (thisHook.const.params[1] && CM.blocks.globals.blocks["Template-file"] && CM.blocks.globals.blocks["Template-file"][thisHook.const.params[1]]) {
-
-    currentTemplate = CM.blocks.globals.blocks["Template-file"][thisHook.const.params[1]].template;
-
-  }
-
-  // Add in fields
-
-  var form = {
-    "template": {
-      "type": "string",
-      "title": "Template filename",
-      "description": "Will be checked for in modules and theme.",
-      "default": currentTemplate
-    },
-  };
-
-  Object.keys(form).forEach(function (formField) {
-
-    data.schema[formField] = form[formField];
-
-  })
-
-  thisHook.finish(true, data);
-
-});
 
 CM.custom_blocks.registerHook("hook_form_render_blockForm_Custom-HTML", 0, function (thisHook, data) {
 
@@ -69,14 +37,8 @@ CM.custom_blocks.registerHook("hook_form_render_blockForm_Custom-HTML", 0, funct
 
 CM.custom_blocks.registerHook("hook_block_render", 0, function (thisHook, data) {
 
-  if (thisHook.const.type === "Template-file") {
+  if (thisHook.const.type === "Custom-HTML") {
 
-    thisHook.finish(true, "Test")
-
-    return true;
-
-  } else if (thisHook.const.type === "Custom-HTML") {
-    
     thisHook.finish(true, thisHook.const.config.contents);
     return true;
 
