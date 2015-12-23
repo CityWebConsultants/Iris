925b4d19fb01a9dc357a3c0329f3a2ae1a4d014a
Filip Hnízdo <filip@bluejumpers.com>
2015-12-23 17:01:29 +0000

    Allow custom code blocks

--- iris_core/modules/core/admin_ui/admin_modules/regions.js
+++ iris_core/modules/core/admin_ui/admin_modules/regions.js
@@ -34,6 +34,10 @@ iris.modules.forms.registerHook("hook_form_render_regions", 0, function (thisHoo
 
     var form = {};
 
+    // Merge in custom blocks
+
+    blocks = blocks.concat(Object.keys(iris.modules.blocks.globals.customBlocks));
+
     // Push in N/A option
 
     blocks.push("None");
@@ -142,25 +146,40 @@ iris.modules.regions.registerHook("hook_frontend_template_parse", 0, function (t
 
           output[regionName].forEach(function (block, index) {
 
-            var settings = block.settings;
+            // Check if custom block
 
-            var block = block.block;
+            if (!iris.modules.blocks.globals.customBlocks) {
 
-            if (block.toLowerCase() === "none") {
+              var settings = block.settings;
 
-              return false;
+              var block = block.block;
+
+              if (block.toLowerCase() === "none") {
+
+                return false;
+
+              }
+
+              var blockName = block.split("|")[0],
+                blockType = block.split("|")[1];
+
+              var config = iris.modules.blocks.globals.blocks[blockType][blockName];
+
+            } else {
+
+              var blockType = "custom",
+                blockName = block,
+                settings = {},
+                config = {};
 
             }
-
-            var blockName = block.split("|")[0],
-              blockType = block.split("|")[1];
 
             var paramaters = {
               index: index,
               id: blockName,
               type: blockType,
               instanceSettings: settings,
-              config: iris.modules.blocks.globals.blocks[blockType][blockName],
+              config: config,
               context: thisHook.const.context
             }
 
@@ -263,7 +282,7 @@ iris.modules.regions.registerHook("hook_block_render", 0, function (thisHook, da
         paths.forEach(function (path) {
 
           var showing = minimatch(currentUrl, path);
-                    
+
         })
 
         thisHook.finish(showing, data);
