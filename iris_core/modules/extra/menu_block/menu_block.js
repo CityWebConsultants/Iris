202b767a779c74b07990a30d62718389ffde6aab
Filip Hnízdo <filip@bluejumpers.com>
2015-12-23 17:25:10 +0000

    Menu block

---
+++ iris_core/modules/extra/menu_block/menu_block.js
@@ -0,0 +1,40 @@
+iris.registerModule("menu_block");
+
+iris.modules.blocks.globals.registerBlockType("menu");
+
+iris.modules.views.registerHook("hook_form_render_blockForm_menu", 0, function (thisHook, data) {
+
+  var availableMenus = {};
+
+  if (iris.configStore.menu) {
+
+    availableMenus = Object.keys(iris.configStore.menu);
+
+  }
+
+  data.schema.menu = {
+    type: "text",
+    "title": "Menu",
+    enum: availableMenus
+  }
+
+  thisHook.finish(true, data);
+
+})
+
+
+iris.modules.menu_block.registerHook("hook_block_render", 0, function (thisHook, data) {
+
+  if (thisHook.const.type === "menu") {
+
+    var config = thisHook.const.config;
+
+    thisHook.finish(true, "[[[menu " + config.menu + "]]]");
+
+  } else {
+
+    thisHook.finish(true, data);
+
+  }
+
+});
