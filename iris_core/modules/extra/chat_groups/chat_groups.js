66acc58cd035e05d4b73c5b6c99690383aa83e63
Malachy Hall Thorpe <me@malachy.org.uk>
2016-01-14 17:11:04 +0000

    Start porting chat group functionality

--- iris_core/modules/extra/chat_groups/chat_groups.js
+++ iris_core/modules/extra/chat_groups/chat_groups.js
@@ -1,1 +1,67 @@
 iris.registerModule('chat_groups');
+
+iris.modules.chat_groups.globals.fetchGroupById = function (groupid) {
+
+  return new Promise(function (yes, no) {
+
+    iris.hook("hook_entity_fetch", thisHook.authPass, null, {
+      queryList: [{
+        entities: ['group'],
+        queries: [{
+          field: 'eid',
+          operator: 'IS',
+          value: groupid
+          }]
+      }]
+    }).then(function (result) {
+
+      yes(result);
+
+    }, function (fail) {
+
+      no();
+
+    });
+
+  });
+
+};
+
+iris.modules.chat_groups.globals.checkMembership = function (groupid, memberid) {
+
+  return new Promise(function (yes, no) {
+
+    iris.hook("hook_entity_fetch", thisHook.authPass, null, {
+      queryList: [{
+        entities: ['group'],
+        queries: [{
+          field: 'eid',
+          operator: 'IS',
+          value: groupid
+          }]
+      }]
+    }).then(function (result) {
+
+      var memberFound = false;
+
+      result.members.forEach(function (element) {
+
+        if (element.userid === memberid) {
+
+          memberFound = true;
+
+        }
+
+      })
+
+      yes(memberFound);
+
+    }, function (fail) {
+
+      no();
+
+    });
+
+  });
+
+}
