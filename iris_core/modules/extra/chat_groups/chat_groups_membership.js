b985310fe5068ab98cf5d98005bc568e502fdee2
Malachy Hall Thorpe <me@malachy.org.uk>
2016-01-15 14:05:33 +0000

    Add removemember API endpoint and hook

--- iris_core/modules/extra/chat_groups/chat_groups_membership.js
+++ iris_core/modules/extra/chat_groups/chat_groups_membership.js
@@ -59,8 +59,64 @@ iris.app.post('/groups/addmember', function (req, res) {
 
 iris.app.post('/groups/removemember', function (req,res) {
 
+  iris.hook('hook_chat_groups_removemember', req.authPass, null, req.body).then(function (pass) {
+
+    res.respond(200, pass);
+
+  }, function (fail) {
+
+    res.respond(500, fail);
+
+  });
+
 });
 
 iris.modules.chat_groups.registerHook("hook_chat_groups_removemember", 0, function (thisHook, data) {
+
+  // Check userid of member is valid
+  if (!(data.member && data.member.userid && typeof data.member.userid === "string")) {
+
+    thisHook.finish(false, "Member not provided, or member does not contain a userid as a string");
+
+  }
+
+  // Fetch requested group
+  iris.modules.chat_groups.globals.fetchGroupById(data.group, thisHook.authPass).then(function (found) {
+
+    var changeMade = false;
+
+    found[0].forEach(function (element, index) {
+
+      if (element === data.member.userid) {
+
+        found[0].members.splice(index, 1);
+
+        changeMade = true;
+
+      }
+
+    });
+
+    if (changeMade) {
+
+      iris.hook("hook_entity_edit", "root", null, found[0]).then(function (success) {
+
+        thisHook.finish(true, success);
+
+      }, function (fail) {
+
+        thisHook.finish(false, "Could not edit entity");
+
+      });
+
+    } else {
+
+      thisHook.finish(false, "The user is not a member of the group")
+
+    }
+
+  }, function (fail) {
+
+  });
 
 });
