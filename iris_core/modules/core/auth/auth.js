3f40e29cac4ce333e90537bbc50ae013412f409c
Filip Hnízdo <filip@bluejumpers.com>
2016-01-31 09:59:10 +0000

    Make auth config directory if doesn't exist

--- iris_core/modules/core/auth/auth.js
+++ iris_core/modules/core/auth/auth.js
@@ -2,6 +2,18 @@
 
 "use strict";
 
+var fs = require('fs');
+
+var mkdirSync = function (path) {
+  try {
+    fs.mkdirSync(path);
+  } catch (e) {
+    if (e.code != 'EEXIST') throw e;
+  }
+}
+
+mkdirSync(iris.configPath + "/" + "auth");
+
 /**
  * @file Methods and hooks for managing user authentication
  */
