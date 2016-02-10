70e22477600f87cf53f01a47a48e2466792a097b
Filip Hnízdo <filip@bluejumpers.com>
2016-02-10 15:18:52 +0000

    Date field

--- iris_core/modules/core/system/system_modules/fields.js
+++ iris_core/modules/core/system/system_modules/fields.js
@@ -139,34 +139,111 @@ iris.modules.entity.registerHook("hook_entity_field_fieldTypeType_form__[Number]
 
 });
 
+iris.modules.entity.registerHook("hook_entity_field_fieldTypeType_form__Date", 0, function (thisHook, data) {
+
+  var value = thisHook.const.value;
+  var fieldSettings = thisHook.const.fieldSettings;
+
+  Number.prototype.padZero = function (len) {
+    var s = String(this),
+      c = '0';
+    len = len || 2;
+    while (s.length < len) s = c + s;
+    return s;
+  }
+
+  if (value) {
+
+    var year = value.getFullYear();
+
+    var month = (value.getMonth() + 1).padZero();
+
+    var day = value.getDate().padZero();
+
+    value = {
+      date: year.toString() + "-" + month.toString() + "-" + day.toString(),
+      time: value.getUTCHours().padZero() + ":" + value.getUTCMinutes().padZero()
+    }
+
+  }
+
+  data = {
+    "type": "object",
+    "title": fieldSettings.label,
+    "default": value,
+    "description": fieldSettings.description,
+    "properties": {
+      "date": {
+        type: "date"
+      },
+      time: {
+        "type": "time"
+      }
+    }
+  }
+
+  thisHook.finish(true, data);
+
+});
+
+iris.modules.entity.registerHook("hook_entity_field_fieldTypeType_form__[Date]", 0, function (thisHook, data) {
+
+  var value = thisHook.const.value;
+  var fieldSettings = thisHook.const.fieldSettings;
+
+  data = {
+    "type": "array",
+    "title": fieldSettings.label,
+    "default": value,
+    "description": fieldSettings.description,
+    "items": {
+      "type": "date"
+    }
+  }
+
+  thisHook.finish(true, data);
+
+});
+
 // Default entity save widgets
 
-iris.modules.entity.registerHook("hook_entity_fieldType_save_String", 0, function (thisHook, data) {
+iris.modules.entity.registerHook("hook_entity_field_fieldTypeType_save__String", 0, function (thisHook, data) {
 
   thisHook.finish(true, thisHook.const.value);
 
 })
 
-iris.modules.entity.registerHook("hook_entity_fieldType_save_Boolean", 0, function (thisHook, data) {
+iris.modules.entity.registerHook("hook_entity_field_fieldTypeType_save__Boolean", 0, function (thisHook, data) {
 
   thisHook.finish(true, thisHook.const.value);
 
 })
 
-iris.modules.entity.registerHook("hook_entity_fieldType_save_[String]", 0, function (thisHook, data) {
+iris.modules.entity.registerHook("hook_entity_field_fieldTypeType_save__[String]", 0, function (thisHook, data) {
 
   thisHook.finish(true, thisHook.const.value);
 
 })
 
-iris.modules.entity.registerHook("hook_entity_fieldType_save_[Number]", 0, function (thisHook, data) {
+iris.modules.entity.registerHook("hook_entity_field_fieldTypeType_save__[Number]", 0, function (thisHook, data) {
 
   thisHook.finish(true, thisHook.const.value);
 
 })
 
-iris.modules.entity.registerHook("hook_entity_fieldType_save_Number", 0, function (thisHook, data) {
+iris.modules.entity.registerHook("hook_entity_field_fieldTypeType_save__Number", 0, function (thisHook, data) {
 
   thisHook.finish(true, thisHook.const.value);
+
+})
+
+iris.modules.entity.registerHook("hook_entity_field_fieldTypeType_save__Date", 0, function (thisHook, data) {
+
+  var date = new Date(thisHook.const.value.date);
+
+  date.setUTCHours(thisHook.const.value.time.split(":")[0]);
+  date.setUTCMinutes(thisHook.const.value.time.split(":")[1]);
+
+  thisHook.finish(true, date);
 
 })
