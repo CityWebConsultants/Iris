def5c0bc0fdb038021ea3685dfd5d1f611f90ddb
pau1m <paul@citywebconsultants.co.uk>
2016-03-10 10:35:34 +0000

    Updating gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,31 @@
-'use strict';
-module.exports = function(grunt){grunt.initConfig({
-  jasmine_node: {
-    api_entity_actions: {
-      options: {
-        coverage: {},
-        forceExit: true,
-        match: '.',
-        matchAll: false,
-        specFolders: ['iris_core/test'],
-        extensions: 'js',
-        specNameMatcher: 'spec',
-        captureExceptions: true,
-        junitreport: {
-          report: false,
-          savePath : './build/reports/jasmine/',
-          useDotNotation: true,
-          consolidate: true
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        jasmine_node: {
+            api_entity_actions: {
+                options: {
+                    coverage: {},
+                    forceExit: false,
+                    match: '.',
+                    matchAll: false,
+                    specFolders: ['iris_core/test'],
+                    extensions: 'js',
+                    specNameMatcher: 'spec',
+                    captureExceptions: true,
+                    junitreport: {
+                        report: true,
+                        savePath: './build/reports/jasmine/',
+                        useDotNotation: true,
+                        consolidate: true
+                    }
+                },
+                src: ['**/*.js']
+            }
         }
-      },
-      src: ['**/*.js']
-    }
-  }
-});
+    });
 
-grunt.loadNpmTasks('grunt-jasmine-node-coverage');
+    grunt.loadNpmTasks('grunt-jasmine-node-coverage');
 
-grunt.registerTask('default', 'jasmine_node');
-}
+    grunt.registerTask('default', 'jasmine_node');
+};
