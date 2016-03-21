50787c415becdbb3c09a0bfa2abe4bda3d0a3863
pau1m <paul@citywebconsultants.co.uk>
2016-03-21 14:27:03 +0000

    Resolving merge conflict

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,7 +36,6 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-jasmine-node-coverage');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-
     grunt.registerTask('default', ['jasmine_node','jshint']);
 
 };
