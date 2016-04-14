6f650ac12ae7fb29dd7a0c96e25b589c57e73230
facascante <chito.cascante@gmail.com>
2016-04-14 20:33:05 +0800

    adjust form submission testing

--- test/ui/CasperEntityIndexSpec.js
+++ test/ui/CasperEntityIndexSpec.js
@@ -2,32 +2,35 @@
 var config = require('../test_config');
 var util = require('../util/schema');
 
-casper.test.begin('Test if schema are properly indexed', 1, function(test) {
+casper.test.begin('Test if schema are properly indexed', 2, function(test) {
   casper.start();
   var login = {
     'input[name = "username"]': config.adminUser.login.username,
     'input[name = "password"]': config.adminUser.login.password
   }
-  util.test.login(login, 'input[name="username"]',function(){});
+  util.test.login(login, 'input[name="username"]', function() { });
   casper.waitForText("This is the Iris administration area", function() {
     var data = {
       'input[name = "fields.unique"]': true,
       'input[name = "fields.required"]': true
     };
-    util.test.updateSchemaField("page","title", data, 'input[name = "fields.unique"]',function(){});
+    util.test.updateSchemaField("page", "title", data, 'input[name = "fields.unique"]', function() { });
   });
   casper.waitForText("Field title saved on entity page", function() {
     var data = {
       'input[name = "title"]': "test title 1",
       'textarea[name = "body"]': 'test body 1'
     };
-    util.test.createEntity("page", data, 'input[name = "title"]',function(){
+    util.test.createEntity("page", data, 'input[name = "title"]', function() {
+
       test.assertExists('form[data-formid="entity"]', "entity form not submitted");
+      test.assertEquals(casper.getCurrentUrl(), 'http://www.iris.local:4000/page/create');
+      
     });
 
   });
 
-  casper.run(function() {    
+  casper.run(function() {
     test.done();
   })
 })
