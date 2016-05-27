d85147e60b703b0370a2bdf6bd99c4b42735a30c
Adam Clarey <adam@citywebconsultants.co.uk>
2016-05-27 11:30:20 +0100

    Revert "Merge pull request #269 from facascante/feature/cardinality_feature"
    This reverts commit 666e469e2b0e01e91622964e7f2d2e8c3c96d139, reversing
    changes made to 8588f2ea3bce6b13f71e8526f1b9d3d44b26db56.

--- db.js
+++ db.js
@@ -332,27 +332,6 @@ iris.dbPopulate = function() {
     Object.keys(schemaConfig.fields).forEach(function(fieldName) {
 
       finalSchema[fieldName] = fieldConverter(schemaConfig.fields[fieldName]);
-      
-      if(finalSchema[fieldName].maxItems){
-        
-        var arrayLimit = function(val){
-          
-          if(Array.isArray(val)){
-            
-            return val.length <= finalSchema[fieldName].maxItems;
-             
-          }
-          else{
-            
-            return true;
-            
-          }
-          
-        };
-    
-        finalSchema[fieldName].validate = [arrayLimit, '{PATH} exceeds the limit of '+ finalSchema[fieldName].maxItems]
-        
-      }
 
     });
 
--- modules/extra/entityUI/schemaUI.js
+++ modules/extra/entityUI/schemaUI.js
@@ -897,7 +897,7 @@ iris.modules.entityUI.registerHook("hook_generate_fieldBasicForm", 0, function (
   data.value.fields = [];
 
   var field = thisHook.context.field;
-  var fieldType = field.type;
+
   delete field.type;
 
   field.about = "<b>Machine name:</b> " + thisHook.context.fieldName + "<br />";
@@ -971,23 +971,6 @@ iris.modules.entityUI.registerHook("hook_generate_fieldBasicForm", 0, function (
       }
 
     }
-  };
-  
-  var readableTypeList = ["[String]", "[Number]", "[Boolean]", "[Date]"];
-
-  if ((Array.isArray(fieldType) && (typeof fieldType[0] === "object")) || (readableTypeList.indexOf(field.readableType) != -1)) {
-
-    data.schema.fields.properties.minItems = {
-      "type": "number",
-      "title": "Minimum entry",
-      "description": "at a minimum, how many record will be accepted for this field?"
-    };
-
-    data.schema.fields.properties.maxItems = {
-      "type": "number",
-      "title": "Maximum entry",
-      "description": "at a maximum, how many record will be accepted for this field?"
-    };
   }
 
   thisHook.pass(data);
