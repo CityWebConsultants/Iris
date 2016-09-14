a0c5eda7dc23bce677afa73b8810a63c67860eae
Filip Hnízdo <filip@bluejumpers.com>
2016-09-14 12:20:28 +0100

    Wait for main server function before loading other modules

--- boot.js
+++ boot.js
@@ -44,7 +44,7 @@ module.exports = function (config) {
 
       });
 
-    },500);
+    }, 500);
 
   };
 
@@ -144,226 +144,230 @@ module.exports = function (config) {
   //Core modules
 
   require('./modules/core/server/server.js');
-  
-  //Require sockets
 
-  require('./modules/core/websockets/websockets.js');
+  iris.modules.server.registerHook("hook_server_ready", 0, function (thisHook, data) {
 
-  require('./modules/core/auth/auth.js');
+    //Require sockets
 
-  require('./modules/core/entity/entity.js');
+    require('./modules/core/websockets/websockets.js');
 
-  require('./modules/core/frontend/frontend.js');
+    require('./modules/core/auth/auth.js');
 
-  require('./modules/core/forms/forms.js');
+    require('./modules/core/entity/entity.js');
 
-  require('./modules/core/filefield/filefield.js');
+    require('./modules/core/frontend/frontend.js');
 
-  require('./modules/core/imagefield/imagefield.js');
+    require('./modules/core/forms/forms.js');
 
-  require('./modules/core/menu/menu.js');
+    require('./modules/core/filefield/filefield.js');
 
-  require('./modules/core/system/system.js');
+    require('./modules/core/imagefield/imagefield.js');
 
-  require('./modules/core/user/user.js');
+    require('./modules/core/menu/menu.js');
 
-  require('./modules/core/paths/paths.js');
+    require('./modules/core/system/system.js');
 
-  require('./modules/core/email/email.js');
+    require('./modules/core/user/user.js');
 
-  try {
+    require('./modules/core/paths/paths.js');
 
-    var file = fs.readFileSync(iris.configPath + '/system/enabled_modules.json', "utf8");
-
-    iris.enabledModules = JSON.parse(file);
-
-  } catch (e) {
-
-    fs.writeFileSync(iris.configPath + '/system/enabled_modules.json', "[]");
-
-    iris.enabledModules = [];
-
-  }
-
-  var glob = require("glob");
-
-  // Check if cache of module paths exists
-
-  var modulePathCache;
-
-  try {
-
-    modulePathCache = JSON.parse(fs.readFileSync(iris.sitePath + "/local/modulePathCache.json", "utf8"));
-
-  } catch (e) {
-
-    modulePathCache = {};
-
-  }
-
-  // Cache object to save with found modules
-
-  var foundModules = {};
-  var toEnable = [];
-
-  iris.enabledModules.forEach(function (enabledModule, index) {
-
-    // Check if path in cache
-
-    var lookup;
+    require('./modules/core/email/email.js');
 
     try {
-      fs.readFileSync(modulePathCache[enabledModule.name]);
-      lookup = [modulePathCache[enabledModule.name]];
+
+      var file = fs.readFileSync(iris.configPath + '/system/enabled_modules.json', "utf8");
+
+      iris.enabledModules = JSON.parse(file);
+
     } catch (e) {
 
-      // Check if module path is a site path
-      var rootParent = iris.rootPath.substring(0, iris.rootPath.length - 7);
-      lookup = glob.sync("{" + rootParent + "/**/" + enabledModule.name + ".iris.module" + "," + iris.sitePath + "/modules/**/" + enabledModule.name + ".iris.module" + "}");
+      fs.writeFileSync(iris.configPath + '/system/enabled_modules.json', "[]");
 
-      lookup.reverse();
+      iris.enabledModules = [];
 
-      if (!lookup.length) {
+    }
 
-        iris.log("error", "error loading module " + enabledModule.name);
+    var glob = require("glob");
+
+    // Check if cache of module paths exists
+
+    var modulePathCache;
+
+    try {
+
+      modulePathCache = JSON.parse(fs.readFileSync(iris.sitePath + "/local/modulePathCache.json", "utf8"));
+
+    } catch (e) {
+
+      modulePathCache = {};
+
+    }
+
+    // Cache object to save with found modules
+
+    var foundModules = {};
+    var toEnable = [];
+
+    iris.enabledModules.forEach(function (enabledModule, index) {
+
+      // Check if path in cache
+
+      var lookup;
+
+      try {
+        fs.readFileSync(modulePathCache[enabledModule.name]);
+        lookup = [modulePathCache[enabledModule.name]];
+      } catch (e) {
+
+        // Check if module path is a site path
+        var rootParent = iris.rootPath.substring(0, iris.rootPath.length - 7);
+        lookup = glob.sync("{" + rootParent + "/**/" + enabledModule.name + ".iris.module" + "," + iris.sitePath + "/modules/**/" + enabledModule.name + ".iris.module" + "}");
+
+        lookup.reverse();
+
+        if (!lookup.length) {
+
+          iris.log("error", "error loading module " + enabledModule.name);
+          return false;
+
+        }
+      }
+
+      var moduleInfoPath = lookup[lookup.length - 1];
+
+      var modulePath = lookup[lookup.length - 1].replace(".iris.module", ".js");
+      var moduleInfo;
+
+      // Add to cache
+
+      foundModules[enabledModule.name] = moduleInfoPath;
+
+      try {
+
+        moduleInfo = JSON.parse(fs.readFileSync(moduleInfoPath));
+
+      } catch (e) {
+
+        // Read config file to check if dependencies satisfied
+
+        console.error("error loading module " + enabledModule.name, e);
         return false;
 
       }
-    }
 
-    var moduleInfoPath = lookup[lookup.length - 1];
+      if (!moduleInfo.weight) {
 
-    var modulePath = lookup[lookup.length - 1].replace(".iris.module", ".js");
-    var moduleInfo;
+        moduleInfo.weight = 0;
 
-    // Add to cache
+      }
 
-    foundModules[enabledModule.name] = moduleInfoPath;
+      toEnable.push({
+        "info": moduleInfo,
+        "path": modulePath,
+        "name": enabledModule.name,
+        "weight": moduleInfo.weight
+      });
 
-    try {
-
-      moduleInfo = JSON.parse(fs.readFileSync(moduleInfoPath));
-
-    } catch (e) {
-
-      // Read config file to check if dependencies satisfied
-
-      console.error("error loading module " + enabledModule.name, e);
-      return false;
-
-    }
-
-    if (!moduleInfo.weight) {
-
-      moduleInfo.weight = 0;
-
-    }
-
-    toEnable.push({
-      "info": moduleInfo,
-      "path": modulePath,
-      "name": enabledModule.name,
-      "weight": moduleInfo.weight
     });
 
-  });
+    toEnable.sort(function (a, b) {
 
-  toEnable.sort(function (a, b) {
+      if (a.weight > b.weight) {
 
-    if (a.weight > b.weight) {
+        return 1;
 
-      return 1;
+      } else if (a.weight < b.weight) {
 
-    } else if (a.weight < b.weight) {
+        return -1;
 
-      return -1;
+      } else {
 
-    } else {
+        return 0;
 
-      return 0;
+      }
 
-    }
+    });
 
-  });
+    toEnable.forEach(function (currentModule) {
 
-  toEnable.forEach(function (currentModule) {
+      var moduleInfo = currentModule.info,
+        modulePath = currentModule.path,
+        name = currentModule.name;
 
-    var moduleInfo = currentModule.info,
-      modulePath = currentModule.path,
-      name = currentModule.name;
+      if (moduleInfo.dependencies) {
 
-    if (moduleInfo.dependencies) {
+        var unmet = [];
 
-      var unmet = [];
+        Object.keys(moduleInfo.dependencies).forEach(function (dep) {
 
-      Object.keys(moduleInfo.dependencies).forEach(function (dep) {
+          if (!iris.modules[dep]) {
 
-        if (!iris.modules[dep]) {
+            unmet.push(dep);
 
-          unmet.push(dep);
+          }
+
+        });
+
+        if (unmet.length) {
+
+          iris.log("error", "Module " + name + " requires the following modules: " + JSON.stringify(unmet));
+          return false;
 
         }
 
-      });
-
-      if (unmet.length) {
-
-        iris.log("error", "Module " + name + " requires the following modules: " + JSON.stringify(unmet));
-        return false;
-
       }
 
-    }
+      iris.registerModule(name, path.parse(modulePath).dir);
 
-    iris.registerModule(name, path.parse(modulePath).dir);
+      try {
 
-    try {
+        require(modulePath);
 
-      require(modulePath);
+      } catch (e) {
 
-    } catch (e) {
+        // Check if module returning other error than file not found
 
-      // Check if module returning other error than file not found
+        if (e.code !== "MODULE_NOT_FOUND") {
 
-      if (e.code !== "MODULE_NOT_FOUND") {
+          iris.log("error", e);
 
-        iris.log("error", e);
+        }
 
       }
-
-    }
-
-  });
-
-  iris.mkdirSync(iris.sitePath + "/" + "local");
-
-  fs.writeFileSync(iris.sitePath + "/local/modulePathCache.json", JSON.stringify(foundModules));
-
-  //Set up database
-
-  require('./modules/core/mongodb/mongodb.js');
-  require('./modules/core/nedb/nedb.js');
-
-  require('./db');
-
-  process.on("dbReady", function () {
-
-    iris.invokeHook("hook_system_ready", "root").then(function () {
-
-      Object.freeze(iris);
 
     });
 
-  });
+    iris.mkdirSync(iris.sitePath + "/" + "local");
 
-  // Send server ready message and get sessions
+    fs.writeFileSync(iris.sitePath + "/local/modulePathCache.json", JSON.stringify(foundModules));
 
-  // There are 2 processes, a master process which allows for persistant sessions and user messages even if
-  // the server is restarted via the admin form or there is a fatal error. The child process is the whole 
-  // system including templates and hooks etc. Changing a module file for instance would require a restart 
-  // to be used by the system. Restarting the child process flushes all files while the parent process
-  // maintains sessions so that everyone isn't logged out.
+    //Set up database
 
-  process.send("started");
+    require('./modules/core/mongodb/mongodb.js');
+    require('./modules/core/nedb/nedb.js');
+
+    require('./db');
+
+    process.on("dbReady", function () {
+
+      iris.invokeHook("hook_system_ready", "root").then(function () {
+
+        Object.freeze(iris);
+
+      });
+
+    });
+
+    // Send server ready message and get sessions
+
+    // There are 2 processes, a master process which allows for persistant sessions and user messages even if
+    // the server is restarted via the admin form or there is a fatal error. The child process is the whole 
+    // system including templates and hooks etc. Changing a module file for instance would require a restart 
+    // to be used by the system. Restarting the child process flushes all files while the parent process
+    // maintains sessions so that everyone isn't logged out.
+
+    process.send("started");
+    
+  })
 
 };
